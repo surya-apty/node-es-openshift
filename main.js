8229be0341b18904317d4807683e0ec1a7874d57
Surya <85492089+surya-apty@users.noreply.github.com>
2023-03-20 18:58:27 +0530

    test

--- main.js
+++ main.js
@@ -1,20 +1,18 @@
 const { Client } = require('@elastic/elasticsearch')
 const client = new Client({
   node: 'http://ec2-13-59-138-158.us-east-2.compute.amazonaws.com:5601',
-  auth: {
-    username: 'elastic',
-    password: 'a1LkrTXTohfgolZS1f_x'
-  }
-//   auth: { apiKey: 'base64EncodedKey' }
 })
+console.log();
+const test = async () => {
+    const getStats = client.nodes.stats();
+    getStats.then(function(o){
+        console.log("started!", e);
+    })
+    .catch(function(e){
+        console.log(e);
+        throw e;
+    });
+};
 
-const result = client.search({
-    index: 'my-index',
-    query: {
-      match: { hello: 'world' }
-    }
-  })
-
-console.log(result);
-
+test();
 console.log('done....!')
